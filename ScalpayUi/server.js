378efe1a09407c3b3dd787370947c585c3356145
Karl Gong <karl.gong@outlook.com>
2018-03-29 11:24:10 +0800

    Change port to 8000

--- ScalpayUi/server.js
+++ ScalpayUi/server.js
@@ -10,7 +10,7 @@ new WebpackDevServer(webpack(config), {
     },
     proxy: {
         "/api": {
-            target: "http://localhost:5432",
+            target: "http://localhost:8000",
             secure: false
         }
     }
